adeec5384919b9b3f5d405453e896f1a8402b435
kallentu <kallentu@hotmail.ca>
2018-01-20 00:43:15 -0500

    Initial, empty project.

---
+++ app/src/main/java/com/lejr/lejr/PreloginActivity.java
@@ -0,0 +1,13 @@
+package com.lejr.lejr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreloginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prelogin);
+    }
+}
