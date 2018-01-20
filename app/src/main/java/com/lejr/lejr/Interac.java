4994053bfcb43db5490051638e6928bf2453db7f
kallentu <kallentu@hotmail.ca>
2018-01-20 17:32:31 -0500

    Bug with token authorization and RetroFit in RequestActivity and Interac

--- app/src/main/java/com/lejr/lejr/Interac.java
+++ app/src/main/java/com/lejr/lejr/Interac.java
@@ -5,11 +5,18 @@ package com.lejr.lejr;
  */
 
 import retrofit2.*;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Headers;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
-public class Interac {
-    private String token;
+public interface Interac {
+    // GET - access token
+    @GET("access-tokens")
+    Call<String> token(@Header("secretKey") String secretKey, @Header("salt") String salt, @Header("thirdPartyAccessId") String thirdPartyAccessId);
 
-    public interface authorize(String accessId){
-        
-    }
+    // POST - send request
+    @POST("/money-requests/send")
+    Call<Void> request(@Header("accessToken") String accessToken, @Header("thirdPartyAccessId") String thirdPartyAccessId, @Header("requestId") String requestId, @Header("deviceId") String deviceId, @Header("apiRegistrationId") String apiRegistrationId);
 }
--- app/src/main/java/com/lejr/lejr/RequestActivity.java
+++ app/src/main/java/com/lejr/lejr/RequestActivity.java
@@ -1,12 +1,20 @@
 package com.lejr.lejr;
 
 import android.content.Intent;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class RequestActivity extends AppCompatActivity {
 
@@ -16,8 +24,8 @@ public class RequestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_request);
     }
 
-    public void submitInformation(View view) {
-
+    public void submitInformation(View view) throws IOException {
+        /*
         EditText amountEditText = (EditText) findViewById(R.id.sending_amount);
         EditText emailEditText = (EditText) findViewById(R.id.sender_email);
 
@@ -29,5 +37,25 @@ public class RequestActivity extends AppCompatActivity {
             TextView email_err = (TextView)findViewById(R.id.req_err_2);
             email_err.setText("Invalid email.");
         }
+        */
+        Retrofit v1 = new Retrofit.Builder()
+                .baseUrl("https://gateway-web.beta.interac.ca/publicapi/api/v1/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+        Interac serviceV1 = v1.create(Interac.class);
+        Call<String> token = serviceV1.token("7dIDt/5D5W6HK8s3gKY/9sAyhPFFxxhk8yZ6V0QVB/U=","jason","CA1TAUCyPsSfqkzX");
+        String accessToken = token.execute().body();
+        Log.e("wut", token.execute().body());
+
+        Retrofit v2 = new Retrofit.Builder()
+                .baseUrl("https://gateway-web.beta.interac.ca/publicapi/api/v2/")
+                .addConverterFactory(ScalarsConverterFactory.create())
+                .build();
+        Interac serviceV2 = v2.create(Interac.class);
+        Call<Void> req = serviceV2.request(accessToken,"CA1TAUCyPsSfqkzX","123", "123","CA1ARhhdGyUQ2kqj");
+
+        //TextView test = (TextView) findViewById(R.id.test);
+        Log.e("wut", accessToken);
+
     }
 }
