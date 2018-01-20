a47d1b353d5fd5463fb91437573cf7908ee69b0a
Jason Chu <jason368@hotmail.ca>
2018-01-20 06:07:29 -0500

    Added email, currency amount validation to activity.

--- app/src/main/java/com/lejr/lejr/RequestActivity.java
+++ app/src/main/java/com/lejr/lejr/RequestActivity.java
@@ -1,7 +1,12 @@
 package com.lejr.lejr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RequestActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class RequestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
+    }
+
+    public void submitInformation(View view) {
+
+        EditText amountEditText = (EditText) findViewById(R.id.sending_amount);
+        EditText emailEditText = (EditText) findViewById(R.id.sender_email);
+
+        if(Integer.parseInt(amountEditText.getText().toString()) < 0){
+            TextView amount_err = (TextView)findViewById(R.id.req_err_1);
+            amount_err.setText("Receiving amount must be at least $0.01.");
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher((EditText)((EditText) findViewById(R.id.sender_email)).getText().toString()).matches()){
+            TextView email_err = (TextView)findViewById(R.id.req_err_2);
+            email_err.setText("Invalid email.");
+        }
     }
 }
