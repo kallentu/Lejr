610e9e3e01b40bc18b307506aabf0486f2cca547
Jason Chu <jason368@hotmail.ca>
2018-01-21 00:31:38 -0500

    Fixed crashing issue with checking if denomination < 0

--- app/src/main/java/com/lejr/lejr/RequestActivity.java
+++ app/src/main/java/com/lejr/lejr/RequestActivity.java
@@ -21,10 +21,6 @@ public class RequestActivity extends AppCompatActivity {
         EditText amountEditText = (EditText) findViewById(R.id.sending_amount);
         EditText emailEditText = (EditText) findViewById(R.id.sender_email);
 
-        if(Integer.parseInt(amountEditText.getText().toString()) < 0){
-            TextView amount_err = (TextView)findViewById(R.id.req_err_1);
-            amount_err.setText("Receiving amount must be at least $0.01.");
-        }
         if(!Patterns.EMAIL_ADDRESS.matcher(((EditText) findViewById(R.id.sender_email)).getText().toString()).matches()){
             TextView email_err = (TextView)findViewById(R.id.req_err_2);
             email_err.setText("Invalid email.");
